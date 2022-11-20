572082c5e1ebfc8ba382e877fe5a1dfff64c52f3
AraHaan <seandhunt_7@yahoo.com>
2022-11-20 00:51:28 -0500

    v0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Since v 0.2.0 aiohttp is required.
 requirements = ['aiohttp']
 
-version = '0.2.0'
+version = '0.2.1'
 
 if not version:
     raise RuntimeError('version is not set')
@@ -14,15 +14,15 @@ with open('README.md') as f:
 setup(name='dbapi',
       author='Decorater',
       author_email='seandhunt_7@yahoo.com',
-      url='https://github.com/AraHaan/dbapi',
-      bugtrack_url='https://github.com/AraHaan/dbapi/issues',
+      url='https://github.com/IzunaDevs/dbapi',
+      bugtrack_url='https://github.com/IzunaDevs/dbapi/issues',
       version=version,
       packages=['dbapi'],
       license='MIT',
       description='Discord Bots API for Discord Bots.',
       long_description=readme,
       maintainer_email='seandhunt_7@yahoo.com',
-      download_url='https://github.com/AraHaan/dbapi',
+      download_url='https://github.com/IzunaDevs/dbapi',
       include_package_data=True,
       install_requires=requirements,
       platforms='Any',
@@ -38,8 +38,10 @@ setup(name='dbapi',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
       ]
